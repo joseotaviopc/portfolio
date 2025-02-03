efa0cb97bd86008d9df5a4c45906241a98e28c85
José Otavio Carvalho <carvalho.jose@gmail.com>
2025-02-02 22:01:08 -0300

    fix: some fixes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: { unoptimized: true },
-  basePath: '/joseotaviopc.github.io',
-  assetPrefix: '/joseotaviopc.github.io/',
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
-import { User, FileText, Bookmark, PenTool } from "lucide-react"
-import Image from "next/image"
-import Link from "next/link"
+import { Bookmark, FileText, PenTool, User } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Hero() {
   return (
@@ -14,8 +14,8 @@ export default function Hero() {
           </h1>
 
           <p className="text-2xl  leading-relaxed max-w-xl">
-            One of my deepest joys comes from equipping creative people with the visual tools they need to further their
-            work.
+            One of my deepest joys comes from equipping creative people with the
+            visual tools they need to further their work.
           </p>
 
           <div className="flex items-center gap-4">
@@ -34,7 +34,9 @@ export default function Hero() {
         </div>
 
         <div className="space-y-6">
-          <h2 className="text-sm uppercase tracking-wider  mb-8">WHERE YOU CAN START</h2>
+          <h2 className="text-sm uppercase tracking-wider  mb-8">
+            WHERE YOU CAN START
+          </h2>
           <nav className="space-y-6">
             <Link
               href="#about"
@@ -43,7 +45,9 @@ export default function Hero() {
               <div className="bg-secondary/90 p-2 rounded">
                 <User className="w-5 h-5" />
               </div>
-              <span className="text-lg group-hover:underline">Learn fun facts about me</span>
+              <span className="text-lg group-hover:underline">
+                Learn fun facts about me
+              </span>
             </Link>
 
             <Link
@@ -53,7 +57,9 @@ export default function Hero() {
               <div className="bg-secondary/90 p-2 rounded">
                 <FileText className="w-5 h-5" />
               </div>
-              <span className="text-lg group-hover:underline">Browse my work history</span>
+              <span className="text-lg group-hover:underline">
+                Browse my work history
+              </span>
             </Link>
 
             <Link
@@ -63,7 +69,9 @@ export default function Hero() {
               <div className="bg-secondary/90 p-2 rounded">
                 <Bookmark className="w-5 h-5" />
               </div>
-              <span className="text-lg group-hover:underline">Read a featured case study</span>
+              <span className="text-lg group-hover:underline">
+                Read a featured case study
+              </span>
             </Link>
 
             <Link
@@ -73,12 +81,13 @@ export default function Hero() {
               <div className="bg-secondary/90 p-2 rounded">
                 <PenTool className="w-5 h-5" />
               </div>
-              <span className="text-lg group-hover:underline">Read a recent blog</span>
+              <span className="text-lg group-hover:underline">
+                Read a recent blog
+              </span>
             </Link>
           </nav>
         </div>
       </div>
     </section>
-  )
+  );
 }
-
